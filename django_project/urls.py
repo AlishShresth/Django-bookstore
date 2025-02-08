b50e83b7867296ba1bde3dbebcadd3354420716d
Alish Shrestha <cresthaalish@gmail.com>
2025-02-08 13:14:50 +0545

    feat(Accounts): Login, Signup

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
-    path("", include("pages.urls")),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")),
+    path("", include("pages.urls"))
 ]
