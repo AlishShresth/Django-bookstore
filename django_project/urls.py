3999a46fcbfa85040e0c9137ceb5dafb955335a8
Alish Shrestha <cresthaalish@gmail.com>
2025-02-08 13:54:22 +0545

    feat(Bootstrap) Add bootstrap

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,9 +21,9 @@ from django.urls import include, path
 urlpatterns = [
     # Django admin
     path("admin/", admin.site.urls),
-    # User management
-    path("accounts/", include("django.contrib.auth.urls")),
     # Local apps
     path("accounts/", include("accounts.urls")),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("pages.urls"))
 ]
