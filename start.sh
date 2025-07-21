56087a316a5b011c9a882085f9f010e2fdced0c0
Alish Shrestha <cresthaalish@gmail.com>
2025-07-21 23:41:26 +0545

    render fixes

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('admin', 'admin@example.com', os.getenv('SUPERUSER_PASSWORD'))" | python manage.py shell
 gunicorn --bind 0.0.0.0:10000 django_project.wsgi:application
